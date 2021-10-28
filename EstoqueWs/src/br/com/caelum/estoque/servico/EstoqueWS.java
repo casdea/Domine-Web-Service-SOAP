215631ee33fbe740b0c04b87ecc2256645492186
casdea <casdea@gmail.com>
2021-10-28 20:03:37 -0300

    Dominando web services SOAP

--- EstoqueWs/src/br/com/caelum/estoque/servico/EstoqueWS.java
+++ EstoqueWs/src/br/com/caelum/estoque/servico/EstoqueWS.java
@@ -2,19 +2,28 @@ package br.com.caelum.estoque.servico;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
-import br.com.caelum.estoque.modelo.item.Item;
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
 
-	  private ItemDao dao = new ItemDao();
+	private ItemDao dao = new ItemDao();
 
-	    public List<Item> getItens() {
+	@WebMethod(operationName = "todosOsItens")
+	@WebResult(name = "itens")
+	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
 
-	        System.out.println("Chamando getItens()");
-	        return dao.todosItens();
-	    }
+		List<Filtro> lista = filtros.getLista();
+
+		System.out.println("Chamando getItens()");
+		return new ListaItens(dao.todosItens(lista));
+	}
 }
